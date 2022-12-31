b10bb7f7ea6e4296ecbd649d2e7abb4df590e115
tom-leamon <leamon@outlook.com>
2022-12-31 08:44:32 -0600

    Expand API

---
+++ pages/entities/index.tsx
@@ -0,0 +1,34 @@
+import { InferGetServerSidePropsType } from 'next'
+import styled from 'styled-components'
+
+type Post = {
+  author: string
+  content: string
+}
+
+const Page = ({ posts }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    // will resolve posts to type Post[]
+  return (<S.App src='http://localhost:1620/admin/content-manager/collectionType/api::entity.entity?page=1&pageSize=100&sort=updatedAt:ASC' allow='camera;microphone' allowFullScreen></S.App>)
+}
+  
+export default Page
+
+export const getServerSideProps = async ({ params }: any) => {
+//   const res = await fetch('https://.../posts')
+console.log(params)
+  const posts: Post[] = []
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+const S = {
+  App: styled.iframe`
+    width: 100%;
+    height: calc(calc(100vh - calc(var(--F_Header_Height) + 300px)) - calc(var(--F_Input_Height) + 1.5rem)); 
+  `
+}
+
