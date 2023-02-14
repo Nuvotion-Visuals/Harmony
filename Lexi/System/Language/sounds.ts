c65d4a4c9c0cfeb7962abe26e35d40d8a4ad89a5
tom-leamon <leamon@outlook.com>
2023-02-14 16:05:49 -0600

    Add JSDocs to sounds.ts

--- Lexi/System/Language/sounds.ts
+++ Lexi/System/Language/sounds.ts
@@ -1,5 +1,14 @@
 type SoundName = 'wake' | 'listen' | 'stop listen' | 'ask' | 'error' | 'send'
 
+/**
+ * Plays an audio file with the specified name from the `/assets/audio/ui` directory.
+ *
+ * @remarks
+ * This method is part of the UI subsystem.
+ *
+ * @param {SoundName} name - The name of the audio file to play. Must be one of `'wake'`, `'listen'`, `'stop listen'`, `'ask'`, `'error'`, or `'send'`.
+ * @returns void
+ */
 export const playSound = (name: SoundName) => {
   const audio = new Audio(`/assets/audio/ui/${name}.wav`)
   audio.play()
