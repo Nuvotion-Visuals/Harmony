68811611fcc6ca42fa37c06daf8b3b96a91394f7
tom-leamon <leamon@outlook.com>
2023-03-19 03:26:08 -0500

    Edit space

--- components/EditSpace.tsx
+++ components/EditSpace.tsx
@@ -1,11 +1,9 @@
-import { Button, Modal, Page, TextInput, generateUUID, Gap, AspectRatio, Box, Item, RichTextEditor, ExpandableLists, ItemProps, LabelColor } from '@avsync.live/formation';
-import { useGenerateGroups } from "Lexi/System/Language/hooks";
-import { language_generateGroups } from "Lexi/System/Language/language-ws";
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-import { useSpaces } from "redux-tk/spaces/hook";
-import styled from "styled-components";
-import { MatrixLoading } from "./MatrixLoading";
+import { Button, TextInput, generateUUID, Gap, AspectRatio, Box, Item, RichTextEditor, ExpandableLists, ItemProps, LabelColor } from '@avsync.live/formation';
+import { useLanguageAPI } from 'Lexi/System/Language/hooks';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useSpaces } from 'redux-tk/spaces/hook';
+import { MatrixLoading } from './MatrixLoading';
 
 import { IconName } from '@fortawesome/fontawesome-svg-core';
 
@@ -39,18 +37,15 @@ interface Props {}
 
 export const EditSpace = ({}: Props) => {
   const router = useRouter();
-
-  const { activeSpace, updateSpace } = useSpaces();
-
-  const [name, set_name] = useState(activeSpace?.name || "");
-  const [description, set_description] = useState(activeSpace?.description || "");
-  const [prompt, set_prompt] = useState(activeSpace?.description || "");
-
+  const { updateSpace, activeSpace, activeSpaceGuid } = useSpaces();
+  const [name, set_name] = useState(activeSpace?.name);
+  const [description, set_description] = useState(activeSpace?.description);
+  const [prompt, set_prompt] = useState('');
   const [url, set_url] = useState<string | undefined>(activeSpace?.previewSrc);
-
-  const [generateGroups, completed, response, loading, error] = useGenerateGroups(description);
-
+  const { language, response, loading, error, completed } = useLanguageAPI('');
   const [suggested, set_suggested] = useState<Suggested>({ groups: [] });
+  const { generateGroups } = language;
+  const [value, set_value] = useState<ExpandableListProps[] | null>(null);
 
   useEffect(() => {
     if (response && completed) {
@@ -61,36 +56,23 @@ export const EditSpace = ({}: Props) => {
     }
   }, [response, completed]);
 
-  const [value, set_value] = useState<ExpandableListProps[] | null>(null);
-
   useEffect(() => {
     if (suggested?.groups) {
       set_value(
         suggested.groups.map((group: Group, i: number) => ({
-          reorderId: `list_${i}`, // Add reorderId here
+          reorderId: `list_${i}`,
           expanded: false,
           value: {
             item: {
-              labelColor: 'none' as LabelColor, // Update labelColor here
+              labelColor: 'none',
               subtitle: group.name,
+              icon: 'caret-down' as IconName,
+              iconPrefix: 'fas',
+              minimalIcon: true,
             },
             list: group.channels.map((channel: Channel) => ({
               subtitle: channel.name,
-              icon: "hashtag" as IconName, // Update icon here
-            })),
-          },
-        }))
-        .map((expandableList: ExpandableListProps) => ({
-          ...expandableList,
-          value: {
-            item: {
-              ...expandableList.value.item,
-              icon: "caret-down" as IconName,
-              iconPrefix: "fas",
-              minimalIcon: true,
-            },
-            list: expandableList.value.list.map((listItem: ItemProps) => ({
-              ...listItem,
+              icon: 'hashtag' as IconName,
             })),
           },
         }))
@@ -99,135 +81,76 @@ export const EditSpace = ({}: Props) => {
   }, [suggested]);
   
   return (
-    <S.new>
-      <Box my={0.25}>
-        <Button
-          icon="chevron-left"
-          iconPrefix="fas"
-          href={`/spaces/${activeSpace?.guid}`}
-          minimal
-        />
-        <Item pageTitle="Edit Space" />
+    <Box wrap>
+      <Box my={0.25} width='100%'>
+        <Item icon='chevron-left' pageTitle='Add Space' href={`/spaces`} />
       </Box>
-
-      <AspectRatio
-        ratio={2}
-        backgroundSrc={url}
-        coverBackground
-      ></AspectRatio>
-      <Box p={0.5}>
-        <Gap disableWrap>
+      <Box px={0.75} wrap>
+        <Gap gap={0.75}>
+          {url && (
+            <AspectRatio ratio={2} backgroundSrc={url} coverBackground />
+          )}
+         
           <TextInput
-            placeholder="Image prompt"
-            compact
+            label='Name'
+            value={name}
+            onChange={(newValue) => set_name(newValue)}
+            autoFocus
+          />
+          <TextInput
+            label='Poster prompt'
             value={prompt}
             onChange={(newValue) => set_prompt(newValue)}
-            hideOutline
-            />
-            <Button
-              icon="bolt-lightning"
-              iconPrefix="fas"
-              secondary
-              circle
-              minimal
-              disabled={prompt === ""}
-              primary={prompt !== ""}
-              blink={!!prompt && !url}
-              onClick={() => {
-                set_url(
-                  `https://lexi.studio/image/prompt/${encodeURIComponent(prompt)}`
-                );
-              }}
-            />
-          </Gap>
-        </Box>
-      
-        <Box px={0.75} wrap>
-          <Gap gap={0.75}>
-            <TextInput
-              label="Name"
-              value={name}
-              onChange={(newValue) => set_name(newValue)}
-              autoFocus
-            />
-            <RichTextEditor
-              placeholder="Description"
-              value={description}
-              onChange={(newValue) => set_description(newValue)}
-            />
-            <Button
-              icon="bolt-lightning"
-              iconPrefix="fas"
-              text={`${completed ? "Reg" : "G"}enerate Channels`}
-              expand
-              secondary
-              disabled={!loading && !description}
-              blink={!!prompt && !url}
-              onClick={() => {
-                generateGroups();
-                set_value(null);
-              }}
-            />
-      
-            {loading && !value?.length ? (
-              <MatrixLoading text={response!} />
-            ) : (
-              <>
-                {value?.map && (
-                  <Box width="100%" wrap>
-                    <ExpandableLists
-                      value={value}
-                      onExpand={(index) =>
-                        set_value(
-                          value.map((item, i) =>
-                            i === index
-                              ? { ...item, expanded: !item.expanded }
-                              : item
-                          )
-                        )
-                      }
-                    />
-                  </Box>
-                )}
-              </>
-            )}
-      
-            <Button
-              hero
-              expand
-              primary={name !== ""}
-              disabled={name === ""}
-              onClick={() => {
-                if (name && activeSpace?.guid) {
-                  updateSpace({
+            buttons={[
+              {
+                icon: 'bolt-lightning',
+                iconPrefix: 'fas',
+                secondary: true,
+                circle: true,
+                minimal: true,
+                disabled: prompt === '',
+                blink: !!prompt && !url,
+                onClick: () => set_url(`https://lexi.studio/image/prompt/${encodeURIComponent(prompt)}`),
+              },
+            ]}
+          />
+          <RichTextEditor
+            placeholder='Description'
+            value={description}
+            onChange={(newValue) => set_description(newValue)}
+          >
+           
+          </RichTextEditor>
+         
+          <Button
+            hero
+            expand
+            primary={name !== ''}
+            disabled={name === ''}
+            onClick={() => {
+              if (name && activeSpace?.guid) {
+                updateSpace({
+                  guid: activeSpace.guid,
+                  space: {
                     guid: activeSpace.guid,
-                    space: {
-                      guid: activeSpace.guid,
-                      groupGuids: activeSpace.groupGuids,
-                      name,
-                      previewSrc: url,
-                      description,
-                    },
-                  });
-                  setTimeout(() => {
-                    router.push(`/spaces/${activeSpace.guid}`);
-                  }, 1);
-                }
-              }}
-              text="Save"
-              icon="save"
-              iconPrefix="fas"
-            />
-      
-            <Box height={4} width="100%" />
-          </Gap>
-        </Box>
-      </S.new>
-    );
-};
-      
-const S = {
-  new: styled.div`
-    position: relative;
-  `
+                    groupGuids: activeSpace.groupGuids,
+                    name,
+                    previewSrc: url,
+                    description,
+                  },
+                });
+                setTimeout(() => {
+                  router.push(`/spaces/${activeSpace.guid}`);
+                }, 1);
+              }
+            }}
+            text='Save'
+            icon='save'
+            iconPrefix='fas'
+          />
+          <Box height={4} width='100%' />
+        </Gap>
+      </Box>
+    </Box>
+  );
 };
