cc972b4c952c20728070b196d83699080f0751b5
tom-leamon <leamon@outlook.com>
2023-04-04 13:58:04 -0500

    [wip] nlp.js add slot filling

--- server/routes/chatbot.ts
+++ server/routes/chatbot.ts
@@ -1,57 +1,48 @@
 import { Request, Response } from 'express';
 // @ts-ignore
-import { NlpManager, Language } from 'node-nlp';
+import { dockStart } from '@nlpjs/basic';
+import path from 'path';
 
-import * as rsvpData from './trainingData/rsvp';
-import * as membershipData from './trainingData/membership';
-// Import more files here...
-
-const manager = new NlpManager({ languages: ['en', 'es'] });
-
-// Add documents and answers for different intents and languages
-[ 
-  { intent: 'RSVP', data: rsvpData },
-  { intent: 'Membership', data: membershipData },
-  // Add more intents and data here...
-].forEach(({ intent, data }) => {
-  Object.entries(data.documents).forEach(([lang, docs]) => {
-    docs.forEach((doc) => {
-      manager.addDocument(lang, doc, intent);
-    });
-  });
-  
-  Object.entries(data.answers).forEach(([lang, answers]) => {
-    Object.entries(answers).forEach(([answerIntent, answer]) => {
-      manager.addAnswer(lang, answerIntent, answer);
-    });
-  });
-});
-// Add default answer
-manager.addAnswer('en', 'None', "I'm sorry, I don't understand.");
-
-(async () => {
-  await manager.train();
-})();
+const context = {};
 
 const chatbotController = async (req: Request, res: Response) => {
   const { message } = req.body;
-  const response = await manager.process(message);
+  const dock = await dockStart({
+    settings: {
+      nlp: {
+        corpora: [
+          path.join(__dirname, './corpa/corpus-en.json'),
+          path.join(__dirname, './corpa/slotFilling-en.json')
+        ]
+      }
+    },
+    'use': ['Basic', 'BuiltinMicrosoft', 'LangEn']
+  });
+
+  const builtin = dock.get('builtin-microsoft');
+  const ner = dock.get('ner');
+  ner.container.register('extract-builtin-??', builtin, true);
+
+  const nlp = dock.get('nlp');
+  await nlp.train();
+  const response = await nlp.process('en', message, context);
+
   const intent = response.intent;
   const answer = response.answer;
   const sentiment = response.sentiment;
+  const entities = response.entities;
 
-  const detectedLanguage: Language = response.locale ?? 'en';
+  const detectedLanguage = response.locale || 'en';
 
   console.log(`\x1b[32mMessage:\x1b[0m \x1b[94m[${detectedLanguage} - ${intent} - ${sentiment.vote}]\x1b[0m \x1b[32m${message}\x1b[0m`);
   console.log(`\x1b[35mAnswer:\x1b[0m \x1b[94m[${detectedLanguage} - ${intent} - ${sentiment.vote}]\x1b[0m \x1b[35m${answer}\x1b[0m`);
+  console.log(entities)
 
+  
   switch (intent) {
-    case 'RSVP':
-    case 'RSVPTime':
-    case 'RSVPGuest':
-    case 'Membership':
-    case 'MembershipLevels':
-    case 'None':
+    case 'agent.birthday':
+    case 'agent.age':
+    // add more intents here
     default:
       res.json({ message: answer, lang: detectedLanguage });
       break;
