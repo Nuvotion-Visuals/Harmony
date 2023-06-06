ec837c3bbdd816153dc2896a99629135952518bc
tom-leamon <leamon@outlook.com>
2023-06-06 18:28:56 -0500

    Update favicon

--- components/ItemMessage.tsx
+++ components/ItemMessage.tsx
@@ -83,7 +83,7 @@ export const ItemMessage = React.memo((props: Props) => {
       <Box width={2} height='100%' wrap>
         <S.Avatar>
           <Avatar
-            labelColor={'gray'}
+            labelColor={'none'}
             src={isLexi ? '/harmony-pink.svg' : undefined}
             icon={isLexi ? undefined : 'user'}
           />
